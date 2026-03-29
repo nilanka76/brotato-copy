527c76e8ccc31945e9c64d6aa59e626fc6616208
Nilanka Biswas <52482681+nilanka76@users.noreply.github.com>
2026-03-29 12:06:56 +0530

    added local changes

---
+++ scenes/Unit/unit.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name Unit
+
+@onready var visuals: Node2D = %Visuals
+@onready var sprite: Sprite2D = %Sprite
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
